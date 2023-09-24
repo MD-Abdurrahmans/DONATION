7d024f3b4ab76fa2bd13fd98b09204572681c26c
artorito22 <abraaz763@gamil.com>
2023-09-24 10:54:46 -0700

    navbar dynamic completed with link added

--- src/layouts/root/Root.jsx
+++ src/layouts/root/Root.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/header/Navbar";
+import Footer from "../../components/footers/Footers";
 
 
 export default function Root() {
   return (
     <div>
-         <h1>fixed</h1>
+          <Navbar></Navbar>
+          <div className="min-h-screen">
+          <Outlet></Outlet>
+          </div>
+           <Footer></Footer>
     </div>
   )
 }
